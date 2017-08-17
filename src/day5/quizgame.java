b166365fa1202b9bc817defa39148872ab7bb5d9
100297 <league@WTS7.attlocal.net>
2017-08-17 14:59:26 -0700

    day4

--- src/day5/quizgame.java
+++ src/day5/quizgame.java
@@ -7,8 +7,10 @@ import javax.swing.JOptionPane;
 public class quizgame {
 	public static void main(String[] args) {
 		int random = new Random().nextInt(4);
+		
 		String A = JOptionPane.showInputDialog("how many strikes do you have in baseball?");
-		if (random == 3) {
+		 int  C= Integer.parseInt(A );
+		if (C== 3) {
 			JOptionPane.showMessageDialog(null, "correct");
 		} else {
 			JOptionPane.showMessageDialog(null, "Wrong");

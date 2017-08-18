fa48a8cd938a8c051c7486fbdeda559d3013bcd9
100297 <league@WTS7.attlocal.net>
2017-08-18 14:47:30 -0700

    day5

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -12,24 +12,25 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
-	final int frameWidth = 600;
-	final int frameHeight = 400;
+	final int frameWidth = 800;
+	final int frameHeight = 600;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
+		maze = ImageIO.read(getClass().getResource("maze.jpg"));
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		new Robot().mouseMove(50, 50);
+		// 3. add a mouse motion listener using:
+		// addMouseMotionListener(this)
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -37,29 +38,36 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
+		// 4. print the mouseColor variable to see what color the mouse is touching
+		System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		int back = -1;
+		int end = -13267144;
+		if (mouseColor == back) {
+			scare();
+		}
+		if (mouseColor == end) {
+			JOptionPane.showMessageDialog(null, "YOU WIN!");
+			// 6. if the mouse falls off the path (if it is on the background)
+		}
+		// call the scare method
 
-		//5. make a variable to hold the background color. 
+		// 10. if the mouse is on the end color
 
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
+		// pop up a message to tell them they won
+
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		// 7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on
+		// freesound.org. Log in as leagueofamazing/code4life.
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("59163__timtube__scream-3.wav"));
 
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+sound.play();
+		// 9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+showScaryImage("Trump.jpg");
 	}
 
 	private void showScaryImage(String imageName) {
@@ -92,9 +100,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
---
+++ src/day5/name.java
@@ -0,0 +1,39 @@
+package day5;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class name {
+public static void main(String[] args) {
+Robot r2d2 = new Robot();
+r2d2.setPenWidth(10);
+r2d2.setRandomPenColor();
+r2d2.setSpeed(10);
+r2d2.penDown();
+r2d2.move(300);
+r2d2.turn(90);
+r2d2.move(90);
+r2d2.turn(90);
+r2d2.move(150);
+r2d2.turn(90);
+r2d2.move(100);
+r2d2.turn(-120);
+r2d2.move(200);
+r2d2.penUp();
+r2d2.turn(-8);
+r2d2.move(50);
+r2d2.penDown();
+r2d2.turn(-120);
+r2d2.move(200);
+r2d2.turn(120);
+r2d2.move(130);
+r2d2.turn(-120);
+r2d2.move(130);
+r2d2.turn(120);
+r2d2.move(200);
+
+
+
+}
+
+
+}
